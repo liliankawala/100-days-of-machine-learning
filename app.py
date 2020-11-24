fc76dec760decc19fd109d5f0f6b31694d2c9327
lilian <kawalalilyy@yahoo.com>
2020-11-24 11:08:14 -0800

    added a function to calculate square

---
+++ app.py
@@ -0,0 +1,14 @@
+
+
+
+def square(number):
+
+    return number * number
+
+
+
+
+for number in range(1, 20):
+    n_square  = square(number)
+    print('{} : {}'.format(number, n_square))
+
---
+++ segmentation.py
@@ -0,0 +1,5 @@
+'''
+
+segmentation by using Reset
+
+'''
